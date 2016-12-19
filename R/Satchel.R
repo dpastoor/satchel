867e8b12e1544ade49a264e0d7b6e8a038c5ca42
Devin Pastoor <devin.pastoor@gmail.com>
2016-12-19 11:50:39 -0500

    very first cut at satchel implementation

---
+++ R/Satchel.R
@@ -0,0 +1,113 @@
+#' Carry around your data
+#' @importFrom R6 R6Class
+#' @name Satchel
+#' @examples
+#' \dontrun{
+#' }
+NULL
+
+#' @export
+Satchel <- R6Class("Satchel",
+                    public =
+                        list(
+                            verbose = NULL,
+                            track = NULL,
+                            initialize = function(
+                                cache_name,
+                                dir = NULL, # where model files should be stored
+                                track = FALSE,
+                                verbose = TRUE,
+                                strict = TRUE
+                            ) {
+                                self$verbose <<- verbose
+                                self$track <<- track
+                                ## cache folder set to same directory as the sourced script
+                                if (is.null(dir)) {
+                                    if (strict) {
+                                        stop("strict mode on - must explicitly declare the directory to store the cache")
+                                    }
+                                    dir <- getwd()
+                                }
+                                dir <- normalizePath(dir, mustWork = FALSE)
+                                if (strict) {
+                                    # if they are 'manually' setting a modeling dir, make sure it exists
+                                    if (!dir.exists(dir)) {
+                                        stop(
+                                            paste(
+                                                "no directory detected at: ",
+                                                dir,
+                                                "please correct the path or create the folder"
+                                            )
+                                        )
+                                    }
+                                } else {
+                                    if (!dir.exists(dir)) {
+                                        dir.create(dir, recursive = TRUE)
+                                        if (self$verbose) {
+                                            message(paste("parent directory created at", dir))
+                                        }
+                                    }
+                                }
+
+                                private$dir <<- dir
+
+                                # cache may not be created yet
+                                cache_folder <-  normalizePath(file.path(dir, cache_name), mustWork = FALSE)
+                                if (!dir.exists(cache_folder)) {
+                                    dir.create(cache_folder, recursive = TRUE)
+                                }
+
+                                private$cache_location <<- cache_folder
+
+                                if (self$verbose) {
+                                    message("global data dir located at: ", private$dir)
+                                    message("cache location named: ", basename(private$cache_location))
+                                }
+                                if (self$track) {
+                                    message("automatic gitignore management not yet implemented, sorry!")
+                                }
+                            },
+                            save = function(data, data_name = NULL) {
+                                if (is.null(data_name)) {
+                                    data_name <- deparse(substitute(data))
+                                }
+                                if (data_name %in% names(private$data)) {
+                                    # don't re-cache if exact object has already been saved
+                                    # for now error on side of memory conservation and
+                                    if (pryr::address(data) == private$data[[data_name]]$mem_address) {
+                                        return(TRUE)
+                                    }
+                                }
+                                saveRDS(data, file.path(private$cache_location, data_name))
+                                private$data[[data_name]] <<- tibble::data_frame(
+                                    name = data_name,
+                                    classes = paste0(class(data), collapse = ", "),
+                                    size = pryr::object_size(data),
+                                    mem_address = pryr::address(data)
+                                )
+
+                            },
+                            use = function(data_name) {
+                                if (is.numeric(data_name)) {
+                                    warning("be careful referencing models by index as changes could result in subtle bugs,
+                                            suggest referring to datasets by name")
+                                }
+                                data <- TRUE # to implement fetching from tree
+                                return(data)
+                                },
+                            report = function(details = FALSE) {
+                                if (details) {
+                                    lapply(private$data, dplyr::glimpse)
+                                } else {
+                                    names(private$data)
+                                }
+
+                            }
+                        ),
+                    private = list(
+                        dir = NULL,
+                        cache_location = NULL,
+                        data = list()
+
+                    )
+)
