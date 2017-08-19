e3494bcca17493338adddaf02b3ec88243deb53e
Devin Pastoor <devin.pastoor@gmail.com>
2017-08-19 15:16:25 -0400

    add some basic tests

---
+++ tests/testthat/test-satchel.R
@@ -0,0 +1,26 @@
+context("satchel")
+
+describe("satchel basics", {
+    tiny_theoph <- head(Theoph)
+    tmpdir_top <- tempdir()
+    tmpdir1 <- file.path(tmpdir_top, "set1")
+    on.exit(unlink(tmpdir1, recursive = TRUE, force = TRUE), add = TRUE)
+    dir.create(tmpdir1, recursive = TRUE)
+    it("initialization works as expected", {
+        expect_error(Satchel$new("tmp", file.path(tmpdir1, "notexist")), "no directory detected at")
+        expect_message(satchel1 <- Satchel$new("tmp", tmpdir1))
+        expect_equal(satchel1$available(), list(tmp = character(0)))
+    })
+    it("can save files", {
+       satchel1$save(tiny_theoph)
+       satchel_files <- dir(tmpdir1, recursive = TRUE)
+       expect_equal(length(satchel_files), 2)
+       satchel1$save(Theoph)
+       satchel_files <- dir(tmpdir1, recursive = TRUE)
+       expect_equal(length(satchel_files), 4)
+    })
+    it("can reload objects that have been saved", {
+       expect_equal(satchel1$use("tiny_theoph"), tiny_theoph)
+       expect_equal(satchel1$use("Theoph"), Theoph)
+    })
+})
